e703a2cc807dad25507d819f3e0a8bdbcb6a2272
Raakesh.R <83003837+raakesh-r@users.noreply.github.com>
2024-06-03 14:56:13 +0530

    Delete src/main/java/com/example/test/demo/repo directory

--- src/main/java/com/example/test/demo/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.test.demo.repo;
-
-import org.apache.catalina.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Long> {
-}
